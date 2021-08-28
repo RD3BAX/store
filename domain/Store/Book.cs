a2fd31c67837e0ced9a4a9e0d0811ac84db32d36
RD3BAX <rd3bax@gmail.com>
2021-08-29 02:47:43 +0300

    Реализовать поиск по ISBN и автору

--- domain/Store/Book.cs
+++ domain/Store/Book.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using System.Text.RegularExpressions;
 
 namespace Store
 {
@@ -6,11 +6,29 @@ namespace Store
     {
         public int Id { get; }
 
+        public string Isbn { get; }
+
+        public string Author { get; }
+
         public string Title { get; }
 
-        public Book(int id, string title)
+        internal static bool IsIsbn(string s)
+        {
+            if (s == null)
+                return false;
+
+            s = s.Replace("-", "")
+                .Replace(" ", "")
+                .ToUpper();
+
+            return Regex.IsMatch(s, @"ISBN\d{10}(\d{3})?$");
+        }
+
+        public Book(int id, string isbn, string author, string title)
         {
             Id = id;
+            Isbn = isbn;
+            Author = author;
             Title = title;
         }
     }
---
+++ domain/Store/BookService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store
+{
+    public class BookService
+    {
+        private readonly IBookRepository _bookRepository;
+
+        public Book[] GetAllByQuery(string query)
+        {
+            if (Book.IsIsbn(query))
+                return _bookRepository.GetAllByIsbn(query);
+
+            return _bookRepository.GetAllByTitleOrAuthor(query);
+        }
+
+        public BookService(IBookRepository bookRepository)
+        {
+            _bookRepository = bookRepository;
+        }
+    }
+}
